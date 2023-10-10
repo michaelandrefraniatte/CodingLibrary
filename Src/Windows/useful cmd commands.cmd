97dd44f479b525756abb8f9d15ea6eb2bd9dbe5d
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-10 11:42:41 +0200

    Delete useful cmd commands.cmd

--- Src/Windows/useful cmd commands.cmd
+++
@@ -1,7 +0,0 @@
-fsutil behavior set memoryusage 2
-bcdedit /set nolowmem off
-bcdedit /set nx alwayson
-bcdedit /set pae forceenable
-bcdedit /set TESTSIGNING OFF
-bcdedit /set NOINTEGRITYCHECKS OFF
-%windir%\SysWOW64\rundll32.exe advapi32.dll,ProcessIdleTasks
